51147f3358ce138e174ee8759e1055296002feaf
Forerunner Simple Git <docker@tech.camp>
2021-06-29 00:36:03 -0400

    Short circuiting in javascript

--- index.js
+++ index.js
@@ -1,90 +1,31 @@
-/* eslint-disable no-constant-condition */
 
 /**
- * If Statements help direct the flow of your code
+ * Short-circuiting is the term applied for combining boolean conditionals 
  */
 
-if (true) {
-	console.log('This will print');
-} else {
-	console.log('This will not print');
-}
 
-if (false) {
-	console.log('This will not printt');
-} else {
-	console.log('This will print');
+let i = 1;
+if (i++ && i++ && i++) {
+	console.log('prints');
 }
+console.log('i: ' + i); // 4
+console.log();
+
+let j = 1;
+if (j++ || j++ || j++) {
+	console.log('prints');
+}
+console.log('j: ' + j); // 2
+console.log();
 
 /**
- * else if can be used to add more cases
+ * starting at 0 the output looks a little different. There are two components at play
+ *  - short-circuiting as described above
+ *  - k++ will increment after the operation evaluates
  */
-
-if (false) {
-	console.log('This will not printt');
-} else if (true) {
-	console.log('This will print');
-} else if (true) {
-	console.log('This will not printt');
+let k = 0;
+if (k++ && k++ && k++) { 
+	console.log('prints');
 }
-
-/**
- * Prints can be nested as well
- */
-
-if (true) {
-	if (true) {
-		console.log('This will printt');
-	}
-} else if (false) {
-	if (true) {
-		console.log('This will not printt');
-	}
-}
-
-/**
- * We can evaluate in if statements
- */
-if (0 === 0) {
-	console.log('This will print');
-} else {
-	console.log('This will not print');
-}
-
-/**
- * We can use variables in 
- */
-const name = 'matt';
-if (name === 'matt') {
-	console.log('This will print');
-} else {
-	console.log('This will not print');
-}
-
-/**
- * we can also evaluate expressions which leads to some interesting examples 
- */
-
-let i = 0;
-if (i++ === 2) {
-	console.log('This will not print');
-} else if (i++ === 2) {
-	console.log('This will not print');
-} else if (i++ === 2) {
-	console.log('This will not print');
-} else if (i++ === 2) {
-	console.log('This will print, this will execute because it is after the evaluation');
-}
-
-i = 0;
-if (++i === 2) {
-	console.log('This will not print');
-} else if (++i === 2) {
-	console.log('This will not print');
-} else if (++i === 2) {
-	console.log('This will print, because it is executed before the evaluation');
-} else if (++i === 2) {
-	console.log('This will not print');
-}
-
+console.log('k: ' + k); // 1
 
