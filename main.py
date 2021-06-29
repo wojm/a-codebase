543c4a294ea4e78a372c7efbc47db3ce47043c48
Forerunner Simple Git <docker@tech.camp>
2021-06-29 00:35:45 -0400

    A file that explains the primitive commands in python

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# String
 
-def hello(name = 'World'):
-	return f'hello { name }'
+my_string = "this is a string"
+print(my_string) # we can display them
+
+# Formated Strings are easy ways to include variables in string
+formated_string = f"let's display our variable in paranthesis ({ my_string }). Pay attention to the spacing."
