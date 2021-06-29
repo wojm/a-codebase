8d32142b4d15eafd938b7b62e8d5e040b6afd34b
Forerunner Simple Git <docker@tech.camp>
2021-06-29 00:35:35 -0400

    show the commands for navigating with bash commands

--- file.sh
+++ file.sh
@@ -1,10 +1,37 @@
 
+echo "This file execute "
 
-# Comments in bash are denoted with a #
-# Nothing after the # will be interpreted
+# List - will list the content for the given directory
+ls # This is useful for showing the existance of a file
 
-# Echo is the print statement for bash
-echo "hello world"
+# we can reference other folders with out ls command by passing it in as the optional argument
+ls ../
 
-# Print statements are a great way good way to test if you can run bash scripts
-echo --help
+# there are also flags that can augment our command
+
+ls -a # list all the files, including those starting with a '.'
+ls -l # shows a long form of the content, useful for seeing access permissions
+
+
+# Path Working Directory - this will tell us where we are on our operating system
+
+# A directory is a folder
+# pwd shows the path (from root) to the current directory you are in
+# It is useful for figuring out where you are
+
+pwd
+
+# Change Directory
+# cd command will help navigate between directories
+# we can use a relative path or an absolute path
+
+cd ./  # will change to our current working directory, aka does nothing
+
+cd ../ # go to the parent directory
+
+cd ./1-navigation # this will go back into this directory
+
+# the below commands will change to your root directory
+# cd ~
+# cd
+
---
+++ solutions.md
@@ -0,0 +1,14 @@
+# Bash Practice
+
+## 0 
+Print Hello World
+```sh
+echo 'Hello World'
+```
+
+## 1
+List the content
+```sh
+ls ~
+```
+
