77cbaad23b766548877e4d51e4820bfd4c7c34e2
Forerunner Simple Git <docker@tech.camp>
2021-08-22 02:04:50 -0400

    Variables in javascript

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
-/**
- * Numbers
- *
- * Only one type of number and it is a floating point (this is a low level detail)
- */
 
-console.log('0: ' + 0 );
-console.log('1: ' + 1 );
-console.log('-1: ' + (-1)  );
-console.log('1.5: ' + 1.5 );
 
 /**
- * Arithmatic Operators
+ * Variables are used to store temporary data. 
+ * 
+ * There are 3 ways to declare variables: const, let, var.
+ * 
+ * Generally, stick with const unless 
  */
-console.log('2 + 3');
-console.log( 2 + 3 );
-console.log('2 * 3');
-console.log( 2 * 3 );
-console.log('2 - 3');
-console.log( 2 - 3 );
-console.log('2 / 3');
-console.log( 2 / 3 );
-console.log('2 ** 3');
-console.log( 2 ** 3 );
 
+/**
+ * const - refers to a constant pointer. Meaning the pointer or value cannot change. 
+ */
+const myVar = 0;
+// myVar now has the value of 0 
+console.log(myVar); // 0
+
+/**
+ * let - refers to a variable that is strictly scoped to the 
+ */
+
+let i = 0;
+console.log(i); // 0
+i = i + 1; // 
+console.log(i); // 1
+
+i++; // another way of writing i = i + 1
+console.log(i); // 2
+i += 1; // another way of writing i = i + 1
+console.log(i); // 3
+
+// i++ operates after the operation it is a part of
+console.log(i++); // 3
+// whereas ++i will happen first
+console.log(++i); // 5
+
+/**
+ * var - can also be update. It has small nuance on how it's defined and how long it exists. 
+ * Generally, do not use, as it the memory it allocates does not get cleaned up
+ * 
+ * https://codeburst.io/difference-between-let-and-var-in-javascript-537410b2d707
+ */
