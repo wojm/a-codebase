a1e832d4e208e5e2df3a85f11704e0002d7f2576
Forerunner Simple Git <docker@tech.camp>
2021-08-21 22:59:44 -0400

    # Names We want to display the names of our classmates on the page.
    Take the names of your classmates and get them to display in your react app.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
 
+
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello Connect 4</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      Matt
+      Faran
+      Joe
+      Michael
     </div>
   );
 }
