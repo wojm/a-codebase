9eb7efa93b563af31a00671722a8b1c00c5c910e
Forerunner Simple Git <docker@tech.camp>
2021-08-22 02:04:54 -0400

    # List names Use a list to display the names. This list is a javascript data structure for holding multiple pieces of information. Here that information is the individual name.
    The key `[` is used to denote the start of a list, `]` is intended to denote the end. In between is a comma separated list elements. The last element should also contain a comma to improve the symmetry of elements in a list. This avoids having to add a comma to a line not affected by the changes we are making.
    
    When displaying the value. We get the value with `names[i]` where i is the index (position in the array).
    
    To display javascript inside of the html, we need to used the escape keys: `{` denotes opening and `}` and to close. Any valid javascript can be escaped out and whatever the result of its evaluation gets displayed.
    
    ## Resources
     - [developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array]

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import "./styles.css";
 
 
 export default function App() {
+  const names = [
+    'Matt',
+    'Faran',
+    'Joe',
+    'Michael',
+  ];
   return (
     <div className="App">
-      <p>Matt</p>
-      <p>Faran</p>
-      <p>Joe</p>
-      <p>Michael</p>
+      <p>{names[0]}</p>
+      <p>{names[1]}</p>
+      <p>{names[2]}</p>
+      <p>{names[3]}</p>
     </div>
   );
 }
