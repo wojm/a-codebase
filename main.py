819f559144c89649d67ae95bea8ef2fba811cdba
Forerunner Simple Git <docker@tech.camp>
2021-08-21 22:59:39 -0400

    # Test data

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
+# Function to find best way to cut a rod of length n
+# where rod of length i has a cost price[i-1]
+def rodCut(price, n):
 
-# We can create a function 
-def greet():
-	print("hello")
+	# base case
+	if n == 0:
+		return 0
 
-greet()
+	maxValue = float('-inf')
 
+	# one by one partition the given rod of length n into two parts of length
+	# (1, n-1), (2, n-2), (3, n-3), .... (n-1 , 1), (n, 0) and take maximum
+	for i in range(1, n):
+
+		# rod of length i has a cost price[i-1]
+		cost = price[i] + rodCut(price, n - i)
+
+		if cost > maxValue:
+			maxValue = cost
+
+	return maxValue
+
+
+if __name__ == '__main__':
+
+	# length = [ 1, 2, 3, 4, 5, 6, 7, 8 ]
+	price = [0, 1, 5, 8, 9, 10, 17, 17, 20]
+
+	# rod length
+	n = 4
+
+	print("Profit is", rodCut(price, n))
