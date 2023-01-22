2b67341bf75f37c5d6d02731f0645bb869dc0dbc
Forerunner Simple Git <docker@tech.camp>
2023-01-22 10:45:18 -0500

    # Grid Given a column we want to create multiple columns. Simple repetition could be used to do this; however, this could lead to a lot of code. We want to use Components to repeat our code.
    To create a component, we define a `function` with the name `Column`. This function should return the values that correspond to the column we created beforehand. We then call this function inside of our `App` function by treating it as an html element `<Column></Column>`. We want 7 of these.
    
    ## Further Reading
     - [https://www.google.com/search?q=react+component](react component) for a reactjs documentation on react components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { useState } from "react";
 import "./styles.css";
+
+function Column() {
+  return (
+    <div className="column">
+      <p className="slot"></p>
+      <p className="slot"></p>
+      <p className="slot"></p>
+      <p className="slot"></p>
+      <p className="slot"></p>
+      <p className="slot"></p>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div className="App">
-      <div className="blue">
-        <div className="slot"></div>
-        <div className="slot"></div>
-        <div className="slot"></div>
-        <div className="slot"></div>
-        <div className="slot"></div>
-        <div className="slot"></div>
+      <h1>Connect 4</h1>
+      <div className="board">
+        <Column></Column>
+        <Column></Column>
+        <Column></Column>
+        <Column></Column>
+        <Column></Column>
+        <Column></Column>
+        <Column></Column>
       </div>
     </div>
   );
