c8b67e3f2af574cd421a3934516352a194078b28
Forerunner Simple Git <docker@tech.camp>
2023-01-22 10:45:06 -0500

    A file that explains functions in python

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-# String
 
-my_string = "this is a string"
-print(my_string) # we can display them
+# We can create a function 
+def greet():
+	print("hello")
 
-# Formated Strings are easy ways to include variables in string
-formated_string = f"let's display our variable in paranthesis ({ my_string }). Pay attention to the spacing."
+greet()
+
